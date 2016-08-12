0b04550ce8aad58e5f06e50b839427156d6671c9
Joshua Wu <joshuawu3@gmail.com>
2016-08-12 10:22:32 +0200

    aesthetics

--- src/parkinG/Testing.java
+++ src/parkinG/Testing.java
@@ -6,6 +6,7 @@ import parkinG.sfpark.templates.SfpAvailability;
 
 public class Testing {
 	public static void main(String[] args) {
+		// 1st testing of inter-layer coordination and communication between Retriever layer and Processor layer
 		RetrieverManager<SfpAvailability> rm = new RetrieverManager<SfpAvailability>();
 		rm.setHttpRetriever("http://api.sfpark.org/sfpark/rest/availabilityservice?lat=37.792275&long=-122.397089&radius=1.0&uom=mile&response=xml");
 		ProcessorManager pm = new ProcessorManager(rm);
